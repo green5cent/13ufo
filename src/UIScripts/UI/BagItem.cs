54a1c2318e63f934d19110fcd5492fa00bd021f1
green5cent <sunmmertime@live.cn>
2016-11-20 15:47:26 +0800

    version1

---
+++ src/UIScripts/UI/BagItem.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System;
+
+public class BagItem : MonoBehaviour{
+    
+    int id;
+    Image img;
+    GameObject obj;
+    void Start()
+    {
+        img = transform.GetComponent<Image>();
+        obj = null;
+        EventTriggerListener.Get(img.gameObject).onRightClick = OnImgClick;
+        EventTriggerListener.Get(img.gameObject).onEnter = OnImgEnter;
+        EventTriggerListener.Get(img.gameObject).onExit = OnImgExit;
+    }
+
+    void OnImgEnter(GameObject go)
+    {
+        if (obj == null)
+        {
+            obj = ObjInfoControl.Instance.SetObj(id,transform);
+        }
+    }
+
+    void OnImgExit(GameObject go)
+    {
+        if(obj!=null)
+        {
+            Destroy(obj);
+        }
+    }
+
+    void OnImgClick(GameObject go)
+    {
+        if(obj!=null)
+            Destroy(obj);
+        ObjectInfo info = ObjectInfoManager.Instance.GetObjectInfo(id);
+
+        if (info._ObjectType==ObjectType.Equip&& EquipControl.Instance.AddItem(id))
+        {
+            ReduceNNum(go);
+        }
+
+        if (info._ObjectType == ObjectType.Drug)
+        {
+            PlayerInfo.Instance.AddHp(info.Hp);
+            ReduceNNum(go);
+        }
+        
+    }
+
+    void ReduceNNum(GameObject go)
+    {
+        string str = transform.GetChild(0).GetComponent<Text>().text;
+        int num = int.Parse(str);
+        num--;
+        transform.GetChild(0).GetComponent<Text>().text = num.ToString();
+        if (num == 0)
+            Destroy(go);
+    }
+
+    public void SetBagItem(int id)
+    {
+        ObjectInfo info = new ObjectInfo();
+        this.id = id;
+        info = ObjectInfoManager.Instance.GetObjectInfo(id);
+        transform.GetComponent<Image>().sprite = Resources.Load<Sprite>(info.IconName);
+        transform.GetChild(0).GetComponent<Text>().text = "";
+
+    }
+
+
+}
---
+++ src/UIScripts/UI/ShopControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class ShopControl : MonoBehaviour
+{
+
+    BagControl bag;
+    Image img;
+    Text coinText;
+    void Start()
+    {
+        bag = GameObject.Find("Bag").GetComponent<BagControl>();
+        img = transform.FindChild("StoreClose").GetComponent<Image>();
+        EventTriggerListener.Get(img.gameObject).onClick = OnClick;
+        coinText = transform.FindChild("Coin").GetComponent<Text>();
+        PlayerInfo.OnInfoChangedEvent += UpdateCoin;
+        UpdateCoin(InfoType.Coin);
+        for (int i=101;i<=112;i++)
+        {
+            Transform obj;
+            obj = Instantiate(Resources.Load<Transform>("ShopItem"));
+            obj.SetParent(GameObject.Find("ShopingGrild").transform);
+            obj.localScale = Vector3.one;
+            obj.localPosition = Vector3.zero;
+            ObjectInfo info = new ObjectInfo();
+            info = ObjectInfoManager.Instance.GetObjectInfo(i);
+            obj.GetComponent<ShopItem>().id = info.Id;
+            obj.FindChild("GoodImg").GetComponent<Image>().sprite = Resources.Load<Sprite>(info.IconName) as Sprite;
+            obj.FindChild("GoodText").GetComponent<Text>().text = info.des;
+            obj.FindChild("GoodCoinText").GetComponent<Text>().text = info.Buy.ToString();
+        }
+    }
+
+    void UpdateCoin(InfoType info)
+    {
+        if (info == InfoType.Coin)
+            coinText.text = PlayerInfo.Instance.Coin.ToString();
+    }
+
+    void OnClick(GameObject go)
+    {
+        transform.DOLocalMove(new Vector3(2000, 0, 0), 0.5f);
+    }
+}
+
+
