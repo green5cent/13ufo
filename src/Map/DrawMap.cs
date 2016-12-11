7ed76c3a1a5d70f341b8bdf0c3ea5d7c2eeb16ab
green5cent <sunmmertime@live.cn>
2016-12-11 23:05:43 +0800

    v1.0

---
+++ src/Map/DrawMap.cs
@@ -0,0 +1,214 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawMap : MonoBehaviour {
+
+    // Use this for initialization
+    Transform bg;
+    Transform land;
+    Transform content;
+
+	void Start () {
+        bg = GameObject.Find("Mud").GetComponent<Transform>();
+        land = GameObject.Find("Land").GetComponent<Transform>();
+        content = GameObject.Find("Content").GetComponent<Transform>();
+        DrawLandform();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void DrawLandform()
+    {
+        DrawContent();
+
+        DrawLand();
+    }
+
+    int SelectMarginDraw(int item, int gridX, int gridY)
+    {
+        if (gridX == 0 || gridX  == Map.width-1 || gridY == 0 || gridY  == Map.height-1)
+            return 2;
+
+        if (gridX - 1 >= 0  && gridX + 1 < Map.width && gridY - 1 >= 0  && gridY + 1 < Map.height)
+        {
+            bool left = GetLand(item, gridX - 1, gridY);
+            bool right = GetLand(item, gridX + 1, gridY);
+            bool down = GetLand(item, gridX, gridY - 1);
+            bool up = GetLand(item, gridX, gridY + 1);
+
+
+            //暂时这样，之后试试递归
+            if (!left && !up && right && down) return 5;
+            if (left && !up && !right && down) return 6;
+            if (!left && up && right && !down) return 7;
+            if (left && up && !right && !down) return 8;
+            if (left && !up && right && down) return 9;
+            if (!left && up && right && down) return 10;
+            if (left && up && right && !down) return 11;
+            if (left && up && !right && down) return 12;
+            if (!left && !up && !right && down) return 13;
+            if (!left && !up && right && !down) return 14;
+            if (!left && up && !right && !down) return 15;
+            if (left && !up && !right && !down) return 16;
+            if (!left && !right && !up && !down) return 19;
+
+            return 2;
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+
+    void DrawLand()
+    {
+        for(int x=0;x<Map.width;x++)
+        {
+            for(int y=0;y<Map.height;y++)
+            {
+                string mud = "mud4";
+
+                DrawOne( mud, x, y, bg);
+
+            }
+        }
+    }
+
+    string SelectBaseDraw(int count,string name)
+    {
+        int i = (int)Random.Range(1f, (float)count);
+
+        return name + i.ToString();
+    }
+
+
+
+    void DrawOne( string name, int x, int y,Transform parent)
+    {
+        Transform item;
+        item = Instantiate(Resources.Load<Transform>("ItemLandform"));
+        item.SetParent(parent);
+        item.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(name) as Sprite;
+        
+        item.position = new Vector3(x * 0.4f, y * 0.4f, 0);
+    }
+
+    void DrawOne(string name, int x, int y, Transform parent,string prefab)
+    {
+        Transform item;
+        item = Instantiate(Resources.Load<Transform>(prefab));
+        item.SetParent(parent);
+        item.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(name) as Sprite;
+
+        item.position = new Vector3(x * 0.4f, y * 0.4f, -2); //content的z轴设为在其它东西的上边
+    }
+
+
+    //判断是否旁边的东西和它的类型是一样的
+    bool GetLand(int item, int neighbourX, int neighbourY)
+    {
+        int t = 0;
+        if(Map.map[neighbourX, neighbourY]==item)
+        {
+            return true;
+        }
+        for (int i = 0; i < Map.landCount; i++)
+        {
+            t = 0;
+            t = Map.map[neighbourX,neighbourY] - i;
+            if (t == Map.tree || t == Map.brushwood || t == Map.stone || t == Map.house || t == Map.wood)
+            {
+              
+                if (i == item )
+                    return true;
+            }
+        }
+        return false;
+    }
+        void DrawContent()
+    {
+        int t=0;
+        int a=0;
+         for (int y = 0; y < Map.height; y++)
+            {
+            for (int x = 0; x < Map.width; x++)
+            {
+                t = 0;
+                 t = Map.map[x, y];
+                DrawLandOther(t, x, y);
+                for (int i = 0; i < Map.landCount; i++) //找到内容
+                {
+                    a = 0;
+                    a = t - i;
+                    if(a == Map.tree)
+                    {
+
+                        DrawOne(MapItemName.tree, x, y,content,"Itemtree");
+                        DrawLandOther(i, x, y);
+                        break;
+                    }
+                    if(a == Map.brushwood)
+                    {
+
+                        DrawOne(MapItemName.brushwood, x, y, content,"Itembrushwood");
+                        DrawLandOther(i, x, y);
+                        break;
+                    }
+                    if(a == Map.wood)
+                    {
+
+                        DrawOne(MapItemName.wood, x, y, content,"Itemwood");
+                        DrawLandOther(i, x, y);
+                        break;
+                    }
+                    if(a == Map.house)
+                    {
+
+                        DrawOne(MapItemName.house, x, y, content,"Itemhouse");
+                        DrawLandOther(i, x, y);
+                        break;
+                    }
+                    if(a == Map.stone)
+                    {
+
+                        DrawOne(MapItemName.stone, x, y, content,"Itemstone");
+                        DrawLandOther(i, x, y);
+                        break;
+                    }
+
+                }
+            }
+        }
+    }
+
+
+    void DrawLandOther(int item, int x, int y)
+    {
+        if (item == Map.grass)
+        {
+            int i = SelectMarginDraw(Map.grass, x, y);
+            if (i != 0)
+            {
+                string grass = MapItemName.grass + i.ToString();
+                DrawOne(grass, x, y, land);
+            }
+        }
+        if (item == Map.water)
+        {
+
+            int i = SelectMarginDraw(Map.water, x, y);
+            if (i != 0)
+            {
+                string water = MapItemName.water + i.ToString();
+                DrawOne(water, x, y, land);
+            }
+
+        }
+    }
+
+    }
---
+++ src/Map/MapLandform.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+  
+
+public class MapLandform : MonoBehaviour
+{
+    
+
+    [Range(0, 60)]
+    public int randomFillPercent0;
+
+    [Range(60, 100)]
+    public int randomFillPercent1;
+
+              
+
+    void Awake()
+    {
+        GenerateMap();
+    }
+
+
+    //void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        GenerateMap();
+    //    }
+    //}
+
+    void GenerateMap()
+    {
+ //       map = new int[width, height];
+        RandomFillMap();
+
+        for (int i = 0; i < 5; i++)
+        {
+           SmoothMap();
+        }
+    }
+
+    void RandomFillMap()
+    {
+
+        System.Random pseudoRandom = new System.Random();
+        for (int x = 0; x < Map.width; x++)
+        {
+            for (int y = 0; y < Map.height; y++)
+            {
+                //暂时先这样，解决总地图四个边不能实现圆滑的问题
+                if (x == 0 || x == Map.width - 1 || y == 0 || y == Map.height - 1 || x==Map.width-2||y==Map.height-2)
+                {
+                    Map.map[x, y] = Map.grass;
+                    continue;
+                }
+                //做判断 随机数在什么区间 
+                int p = pseudoRandom.Next(0, 100);
+                    if (p <= randomFillPercent0)
+                        Map.map[x, y] = Map.water;     //等于水
+                    else if (p < randomFillPercent1)
+                        Map.map[x, y] = Map.grass;   //等于草地
+                    else
+                        Map.map[x, y] = Map.mud;  //等于泥地
+                }
+            }
+        }
+    
+
+    void SmoothMap()
+    {
+        for (int x = 0; x < Map.width; x++)
+        {
+            for (int y = 0; y < Map.height; y++)
+            {
+
+                int neighbourStoteTiles = GetSurroundingWallCount(x, y)[0];
+                int neighbourCementTiles = GetSurroundingWallCount(x, y)[1];
+
+                if (neighbourStoteTiles >= 4)    
+                    Map.map[x, y] = Map.water;
+                else if (neighbourCementTiles >= 4)
+                    Map.map[x, y] = Map.mud;
+                else
+                    Map.map[x, y] = Map.grass;
+
+            }
+        }
+    }
+
+    int[] GetSurroundingWallCount( int gridX, int gridY)
+    {
+        int[] Count = new int[2];
+        Count[0] = Count[1] = 0;
+
+        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
+        {
+            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
+            {
+                if (neighbourX >= 0 && neighbourX < Map.width && neighbourY >= 0 && neighbourY < Map.height)
+                {
+                    if (neighbourX != gridX || neighbourY != gridY)
+                    {
+                        if (Map.map[neighbourX, neighbourY] == Map.water)
+                            Count[0]++;
+                        if (Map.map[neighbourX, neighbourY] == Map.mud)
+                            Count[1]++;
+                    }
+                }
+            }
+        }
+        return Count;
+    }
+
+        
+    //void OnDrawGizmos()
+    //{
+    //    if (Map.map != null)
+    //    {
+    //        for (int x = 0; x < Map.width; x++)
+    //        {
+    //            for (int y = 0; y < Map.height; y++)
+    //            {
+    //                if (Map.map[x, y] == (int)LandformState.Stone)
+    //                    Gizmos.color = Color.blue;
+    //                else if (Map.map[x, y] == (int)LandformState.Cement)
+    //                    Gizmos.color = Color.gray;
+    //                else
+    //                    Gizmos.color = Color.green;
+    //                Vector3 pos = new Vector3(-Map.width / 2 + x + .5f , -Map.height / 2 + y + .5f,0 );
+    //                Gizmos.DrawCube(pos, Vector3.one);
+    //            }
+    //        }
+    //    }
+    //}
+}
